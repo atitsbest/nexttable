42d7ab2fbf18456d9cbf7194c443c8fd76c774c0
Meissner <smeissner@dccsintra.net>
2021-01-28 21:53:35 +0100

    changed image, made it smaller

--- src/components/explosion.js
+++ src/components/explosion.js
@@ -6,7 +6,7 @@ import Img from "gatsby-image"
 const Explosion = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "explosion.png" }) {
+      placeholderImage: file(relativePath: { eq: "xtable.png" }) {
         childImageSharp {
           fluid(maxWidth: 1400) {
             ...GatsbyImageSharpFluid
@@ -22,14 +22,16 @@ const Explosion = () => {
 
   return (
     <div className="container mx-auto mt-8">
-      <div className="relative">
+      <div className="relative sm:flex sm:justify-center">
         <Img
           fluid={{
             ...data.placeholderImage.childImageSharp.fluid,
             aspectRatio: 21 / 9,
           }}
+          className="mx-auto"
+          style={{ width: "80%" }}
         />
-        <div className="sm:flex sm:absolute inset-0 sm:grid sm:grid-cols-2 sm:gap-8 mt-16 mb-16">
+        <div className="sm:flex sm:absolute inset-0 sm:grid sm:grid-cols-2 sm:gap-8 mt-4 mb-16">
           <div>
             <div className="ml-8 grid sm:grid-rows-3 sm:gap-8">
               <Item
