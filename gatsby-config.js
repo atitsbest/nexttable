e04c35af81a3fa2d5b378524c8493b8321f07623
Meissner <smeissner@dccsintra.net>
2021-01-30 21:09:18 +0100

    google analytics done the gatsby way.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
       },
     },
     "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-JGMTW281L9"],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
